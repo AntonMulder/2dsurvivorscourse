fdd000791ca04838bd7f6c42f0ac0f3d47222581
Anton Mulder <anton.mulder@wearespindle.com>
2024-12-29 23:01:16 +0100

    10. Targeting Enemies With Sword Ability

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const MAX_RANGE = 150
+
 @export var sword_ability: PackedScene
 
 
@@ -10,10 +12,29 @@ func _ready() -> void:
 
 func on_timer_timeout():
 	var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
-
 	if player == null:
 		return
-	var sword_instance = sword_ability.instantiate() as Node
+	var enemies: Array[Node] = get_tree().get_nodes_in_group("enemy")
 
+	enemies = enemies.filter(
+		func(enemy: Node2D):
+			return (
+				enemy.global_position.distance_squared_to(player.global_position)
+				< pow(MAX_RANGE, 2)
+			)
+	)
+
+	if enemies.size() == 0:
+		return
+
+	enemies.sort_custom(
+		func(a: Node2D, b: Node2D):
+			var a_distance = a.global_position.distance_squared_to(player.global_position)
+			var b_distance = b.global_position.distance_squared_to(player.global_position)
+
+			return a_distance < b_distance
+	)
+
+	var sword_instance = sword_ability.instantiate() as Node
 	player.get_parent().add_child(sword_instance)
-	sword_instance.global_position = player.global_position
+	sword_instance.global_position = enemies[0].global_position
