0ca1a599a09775790fe4fa1f544a956d27796028
Anton Mulder <anton.mulder@wearespindle.com>
2025-01-14 21:56:36 +0100

    18. Creating a Health Component

---
+++ scenes/component/health_component.gd
@@ -0,0 +1,22 @@
+class_name HealthComponent
+extends Node
+
+signal died
+
+@export var max_health: float = 10
+var current_health
+
+
+func _ready():
+	current_health = max_health
+
+
+func damage(damage_amount: float):
+	current_health -= damage_amount
+	Callable(check_health).call_deferred()
+
+
+func check_health():
+	if current_health <= 0:
+		died.emit()
+		owner.queue_free()
