dc79c7de0199b19851ad026fb7c019019f799fee
Anton Mulder <anton.mulder@wearespindle.com>
2025-01-26 17:43:10 +0100

    23. Enabling Upgrade Selection

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -20,6 +20,7 @@ func on_level_up(_current_level: int) -> void:
 	var upgrade_screen_instance: UpgradeScreen = upgrade_screen_scene.instantiate()
 	add_child(upgrade_screen_instance)
 	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
 
 
 func apply_upgrade(upgrade: AbilityUpgrade) -> void:
@@ -28,3 +29,9 @@ func apply_upgrade(upgrade: AbilityUpgrade) -> void:
 		current_upgrades[upgrade.id] = {"resource": upgrade, "quantity": 1}
 	else:
 		current_upgrades[upgrade.id]["quantity"] += 1
+
+	print(current_upgrades)
+
+
+func on_upgrade_selected(upgrade: AbilityUpgrade) -> void:
+	apply_upgrade(upgrade)
