8e4f272e4dd775567f04bebe9a7ae764da7b22c1
Anton Mulder <anton.mulder@wearespindle.com>
2025-02-04 22:29:39 +0100

    30. Increase Difficulty Over Time

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,12 +1,27 @@
-extends Node
+class_name ArenaTimeMagner extends Node
+
+signal arena_difficulty_increased(arena_difficulty: int)
+
+const DIFFICULTY_INTERVAL = 5
 
 @export var end_screen_scene: PackedScene
 
-@onready var timer = $Timer
+var arena_difficulty = 0
+
+@onready var timer: Timer = $Timer
 
 
 func _ready() -> void:
     timer.timeout.connect(on_timer_timeout)
+
+
+func _process(_delta: float) -> void:
+    var next_time_target = (
+        timer.wait_time - ((arena_difficulty + 1) * DIFFICULTY_INTERVAL)
+    )
+    if timer.time_left <= next_time_target:
+        arena_difficulty += 1
+        arena_difficulty_increased.emit(arena_difficulty)
 
 
 func get_time_elapsed() -> float:
--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -3,13 +3,24 @@ extends Node
 const SPAWN_RADIUS = 375
 
 @export var basic_enemy_scene: PackedScene
+@export var arena_time_manager: ArenaTimeMagner
+
+var base_spawn_time: float = 0.0
+
+@onready var timer: Timer = $Timer
 
 
 func _ready() -> void:
-    $Timer.timeout.connect(on_timer_timeout)
+    base_spawn_time = timer.wait_time
+    timer.timeout.connect(on_timer_timeout)
+    arena_time_manager.arena_difficulty_increased.connect(
+        on_arena_difficulty_increased
+    )
 
 
 func on_timer_timeout():
+    timer.start()
+
     var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
 
     if player == null:
@@ -28,3 +39,9 @@ func on_timer_timeout():
     var enemy: CharacterBody2D = basic_enemy_scene.instantiate()
     entities_layer.add_child(enemy)
     enemy.global_position = spawn_position
+
+
+func on_arena_difficulty_increased(arena_difficulty: int) -> void:
+    var time_off = (.1 / 12) * arena_difficulty
+    print(time_off)
+    timer.wait_time = max(base_spawn_time - time_off, 0.1)
