d1a57ccc708af598da1c8697724fd198545eb2a3
Anton Mulder <anton.mulder@wearespindle.com>
2025-02-21 21:41:38 +0100

    44. Adding Floating Damage Text

--- scenes/component/hurtbox_component.gd
+++ scenes/component/hurtbox_component.gd
@@ -2,6 +2,10 @@ class_name HutboxComponent extends Area2D
 
 @export var health_component: HealthComponent
 
+var floating_text_scene: PackedScene = preload(
+    "res://scenes/ui/floating_text.tscn"
+)
+
 
 func _ready() -> void:
     area_entered.connect(on_area_entered)
@@ -16,3 +20,10 @@ func on_area_entered(other_area: Area2D) -> void:
 
     var hitbox_component: HitboxComponent = other_area
     health_component.damage(hitbox_component.damage)
+    var floating_text: FloatingText = floating_text_scene.instantiate()
+    get_tree().get_first_node_in_group("foreground_layer").add_child(
+        floating_text
+    )
+
+    floating_text.global_position = global_position + (Vector2.UP * 16)
+    floating_text.start(str(hitbox_component.damage))
